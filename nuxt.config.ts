bd7217bf45c260496b50f684ba0ec4606d37a196
Q <q@misell.cymru>
2023-01-16 16:17:09 +0100

    target to glauca.space

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -86,7 +86,7 @@ export default defineNuxtConfig({
       buildInfo: {} as BuildInfo, // set in build-env module
       pwaEnabled: !isDevelopment || process.env.VITE_DEV_PWA === 'true',
       translateApi: '',
-      defaultServer: 'mas.to',
+      defaultServer: 'glauca.space',
     },
     storage: {
       driver: isCI ? 'cloudflare' : 'fs',
